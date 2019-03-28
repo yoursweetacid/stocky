0c4f8bf6f8e9de886b7c86e08598f18fb81199fa
Nikita Levchenko <nikmurrey@gmail.com>
2019-03-28 16:46:52 +0300

    Update sign_in_error.php

--- sign_in_error.php
+++ sign_in_error.php
@@ -1,6 +1,6 @@
 <body class="bg-near-white">
 <article class="pa5 mt6 mb6">
-<p class="black-80 sans-serif fw4 f4">There is no such registered user, we're sorre for that</p>
+<p class="black-80 sans-serif fw4 f4">There is no such registered user, we're sorry for that</p>
 <a href='sign_in_form.php' class="baskerville link green no-underline">Go back to form</a>
 </article>
 </body>
